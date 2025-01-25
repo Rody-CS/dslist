75444b4e685584ee4ec17b1a390367a0f30fb4a8
Rodrigo Castro <rodrigocd28@gmail.com>
2025-01-25 17:44:29 -0300

    Properties, Game, ORM

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,64 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tb_game")
+public class Game {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game() {
+    }
+
+    public Game(long id, String title, Integer year, String genre, String platform, String imgUrl,
+            String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platform = platform;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Game other = (Game) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+}
